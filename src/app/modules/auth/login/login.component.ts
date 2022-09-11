c9e8f6ffec50db06224158bba0cdc237c3130b15
Will-h0219 <wherrera0219@gmail.com>
2022-09-10 23:59:18 -0500

    login page added

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup = this.fb.group({
+    userEmail: ['', [Validators.required]],
+    userPassword: ['', [Validators.required]]
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    console.log(this.loginForm.value);
+    //this.router.navigateByUrl('/dashboard');
+  }
 }
